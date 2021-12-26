e4c89c6b1ee03724d80dac813b6e14250cde9e21
21chelika <85401286+21chelika@users.noreply.github.com>
2021-12-26 13:33:58 +0700

    Add files via upload

---
+++ GUI.java
@@ -0,0 +1,43 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class GUI extends JFrame{
+
+    GridLayout gl = new GridLayout(4,6);
+    JButton b1 = new JButton ("");
+    
+    GUI () {
+        super("Graphical User Interface");
+        setSize(400,200);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        getContentPane().setBackground(Color.CYAN);
+        setLocationRelativeTo(null);
+        setLayout(gl);
+        add(" West ",b1);
+        add(new JButton(" 1 "));
+        add(new JButton(" 2 "));
+        add(new JButton(" 3 "));
+        add(new JButton(" 4 "));
+        add(new JButton(" 5 "));
+        add(new JButton(" 6 "));
+        add(new JButton(" 7 "));
+        add(new JButton(" 8 "));
+        add(new JButton(" 9 "));
+        add(new JButton(" 0 "));
+        add(new JButton(" + "));
+        add(new JButton(" - "));
+        add(new JButton(" * "));
+        add(new JButton(" / "));
+        add(new JButton(" = "));
+        add(new JButton(" % "));
+        add(new JButton(" Mod "));
+        add(new JButton(" Exit "));
+        setVisible(true);
+        
+    }
+    public static void main(String[] args) {
+        GUI form= new GUI();
+        
+    }
+    
+}
